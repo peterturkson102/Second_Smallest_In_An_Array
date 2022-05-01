c27fec547bcf2297b628187a36c12ece2d5848f1
peterturkson102 <peterturkson102@gmail.com>
2022-05-01 00:04:27 +0000

    Updated naming and structure

--- src/SecondSmallestInArray.java
+++ src/SecondSmallestInArray.java
@@ -16,8 +16,10 @@ public class SecondSmallestInArray{
         return a[1];//2nd element because index starts from 0
     }
     public static void main(String args[]){
-        int a[]={1,2,5,6,3,2};
-        int b[]={44,66,99,77,33,22,55};
-        System.out.println("Second smallest: "+getSecondSmallest(a,6));
-        System.out.println("Second smallest: "+getSecondSmallest(b,7));
+        int arrayA[]={1,2,5,6,3,2};
+        int arrayB[]={44,66,99,77,33,22,55};
+        int aTotal = arrayA.length;
+        int bTotal = arrayB.length;
+        System.out.println("Second smallest number of Array arrayA: "+getSecondSmallest(arrayA,aTotal));
+        System.out.println("Second smallest number of Array arrayB: "+getSecondSmallest(arrayB,bTotal));
     }}
